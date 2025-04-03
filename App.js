0a0635a8d7d9db4ebabb471ddabe3c3c1ec578e7
ayyan656 <124901068+ayyan656@users.noreply.github.com>
2025-04-03 15:38:50 +0500

    Update App.js

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ store.on("error", function (error) {
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://crud-app-with-auth.onrender.com/", credentials: true }));
 app.use(cookieParser());
 
 // Express Session Middleware (Now using MongoDB)
